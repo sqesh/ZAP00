a52ff9658e16304b5ef5ee41445f8ae9445fa7d1
sqesh <krzysiek1775@gmail.com>
2019-05-25 23:39:05 +0200

    02 - create

--- functions.h
+++ functions.h
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <math.h>
 #include "matrix.h"
 
 template <typename Type>
@@ -25,6 +26,31 @@ void saveMatrix(Matrix<Type> input, std::string filename)
                 outputFile << '\n';
             }
         }
+        outputFile.close();
+    }
+}
+
+template <typename Type>
+void saveMatrixForMatLab(Matrix<Type> input, std::string filename)
+{
+    std::ofstream outputFile;
+    outputFile.open(&filename[0]);
+    if(outputFile.is_open())
+    {
+        outputFile << '[';
+        for(unsigned height=0; height<input.height(); height++)
+        {
+            for(int width=0; width<input.width(); width++)
+            {
+                outputFile << input[height][width];
+                if(width<(input.width()-1))outputFile << ' ';
+            }
+            if(height<(input.height()-1))
+            {
+                outputFile << ';';
+            }
+        }
+        outputFile << ']';
         outputFile.close();
     }
 }
@@ -55,6 +81,7 @@ Matrix<Type> loadMatrix(std::string filename)
             }
         }
         inputFile.close();
+        return output;
     }
 }
 
@@ -71,6 +98,37 @@ void printMatrix(Matrix<Type> input)
     }
 }
 
+template <typename Type>
+Matrix<Type> createMatrixA(unsigned n, double thermalConductivity, double thermalTransfer, double area, double lenght)
+{
+    Matrix<Type> output(n,n);
+    double dx = lenght / n;
+    double temp1 = (thermalConductivity * area) /dx;
+    double temp2 = 2*thermalTransfer*sqrt(3.14*area)*dx;
+    for(unsigned height=0; height<output.height() ; height++)
+    {
+        for(unsigned width=0; width<output.width() ; width++)
+        {
+            if((height==0 && width==0) || (height==output.height()-1 && width== output.width()-1)) output[height][width] = 1;
+            else if((width == height-1 || width == height+1)&&(height!=0 && height!=output.height()-1)) output[height][width] = -temp1;
+            else if(width==height) output[height][width] = (2*temp1) + temp2;
+            else  output[height][width] = 0;
+        }
+    }
+}
 
+template <typename Type>
+Matrix<Type> createMatrixA(unsigned n, double thermalTransfer, double area, double lenght, double temperatureLeftSide, double temperatureRightSIde, double temperatureOutside)
+{
+    Matrix<Type> output(n,n);
+    double dx = lenght / n;
+    double temp2 = 2*thermalTransfer*sqrt(3.14*area)*dx;
+    for(unsigned height ; height < output.height(); height++)
+    {
+        if(height==0) output[height][0] = temperatureLeftSide;
+        else if(height==output.height()-1) output[height][0] = temperatureRightSIde;
+        else output[height][0] = temp2*temperatureOutside;
+    }
+}
 
 #endif // FUNCTIONS_H
